dc0320bda599be39f776e5f5a7fa11ec5572ea9a
Specialist969 <pawel.musial@microsoft.wsei.edu.pl>
2023-04-23 12:19:20 +0200

    Ciąg dalszy

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using System.Reflection;
 using WebApp;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -13,7 +15,25 @@ builder.Services.AddDbContext<StudentContext>(opt =>
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c => {
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "Student API",
+        Description = "demo Student API",
+        Version = "v1",
+        Contact = new OpenApiContact
+        {
+            Name = "Example Contact",
+            Url = new Uri("https://adres_strony")
+        },
+    });
+});
+
+var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+c.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
+
+
+
 
 var app = builder.Build();
 
@@ -21,7 +41,11 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Student API V1");
+    });
+    ;
 }
 
 app.UseHttpsRedirection();
--- StudentsController.cs
+++ StudentsController.cs
@@ -79,6 +79,22 @@ namespace WebApp
             return NoContent();
         }
 
+        /// <summary>
+        /// Wprowadź studenta
+        /// </summary>
+        /// <param name="student"></param>
+        /// <returns>A newly created Student</returns>
+        /// <remarks>
+        /// Sample request:
+        ///
+        /// POST /Student
+        /// {
+        /// "id": 1,
+        /// "name": "Anna",
+        /// "surnname": "Zablotni"
+        /// }
+        ///
+        /// </remarks>
         // POST: api/Students
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPost]
@@ -94,6 +110,11 @@ namespace WebApp
             return CreatedAtAction("GetStudent", new { id = student.Id }, student);
         }
 
+        /// <summary>
+        /// Usuwanie Studentow z listy.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         // DELETE: api/Students/5
         [HttpDelete("{id}")]
         public async Task<IActionResult> DeleteStudent(int id)
